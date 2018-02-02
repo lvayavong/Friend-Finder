c36224d1bb7fb57b9fe3118de6cc03b42d7adf74
unknown <londonvayavong@gmail.com>
2018-02-02 01:10:53 -0800

    adjusted html files and routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,12 +6,70 @@
 // ===============================================================================
 
 var friendsArray = [
-    {
-      customerName: "Saima",
-      customerEmail: "saima@example.com",
-      phoneNumber: "000-000-0000",
-      customerID: "saimacool"
-    }
+  {
+    "name": "Captain America",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+   {
+    "name": "Thor",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  }
+   {
+    "name": "Darkman",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+  }
+   {
+    "name": "Superman",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      3,
+      2,
+      1,
+      5,
+      4,
+      3,
+      3,
+      1,
+      2,
+      5
+    ]
+  }
   ];
   
   
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,11 @@
 ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// We are linking our routes to a series of "public" sources.
+// These public sources hold arrays of information on table-public, waitinglist, etc.
 // ===============================================================================
-
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var path = require ("path");
+var homeData = require("../public/home.html");
+var surveyData = require("../public/survey.html");
 
 
 // ===============================================================================
@@ -13,51 +13,40 @@ var waitListData = require("../data/waitinglistData");
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
+  // public GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // (ex: localhost:PORT/public/admin... they are shown a JSON of the public in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/public/home", function(req, res) {
+    res.json(home);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
+  app.get("/public/survey", function(req, res) {
+    res.json(surveyData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
+  // public POST Requests
+  // Below code handles when a user submits a form and thus submits public to the server.
+  // In each of the below cases, when a user submits form public (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // (ex. User fills out a reservation request... this public is then sent to the server...
+  // Then the server saves the public to the home array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/public/home", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+    if (home.length < 5) {
+      home.push(req.body);
       res.json(true);
     }
     else {
-      waitListData.push(req.body);
+      surveyData.push(req.body);
       res.json(false);
     }
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });
 };
